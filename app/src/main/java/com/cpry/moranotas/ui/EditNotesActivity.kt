5810c63bc185098ac9c9b9984853296ff38dd283
raul-oliveira <mrraul65@gmail.com>
2019-03-19 22:59:37 -0300

    ajuste no git ignore

---
+++ app/src/main/java/com/cpry/moranotas/ui/EditNotesActivity.kt
@@ -0,0 +1,4 @@
+package com.cpry.moranotas.ui
+
+class EditNotesActivity {
+}
---
+++ app/src/main/java/com/cpry/moranotas/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.cpry.moranotas.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cpry.moranotas.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
